4560511e3f4a680f054ae252a488c1f789002e23
Alireza Mousavi <mousaviali1381@gmail.com>
2026-07-11 00:52:00 +0400

    feat(tui): add countdown timer

--- cmd/ttype/main.go
+++ cmd/ttype/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/alirezaudev/ttype/internal/tui"
 	tea "github.com/charmbracelet/bubbletea"
@@ -11,7 +12,8 @@ import (
 func main() {
 
 	p := tea.NewProgram(tui.TestModel{
-		Text: "Yes, man has a hard life. I think the best definition of man is this: a creature who can grow accustomed to anything.",
+		Text:     "Yes, man has a hard life. I think the best definition of man is this: a creature who can grow accustomed to anything.",
+		Duration: 10 * time.Second,
 	})
 
 	_, err := p.Run()
--- internal/tui/test_model.go
+++ internal/tui/test_model.go
@@ -1,7 +1,9 @@
 package tui
 
 import (
+	"fmt"
 	"strings"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -17,15 +19,23 @@ type TestModel struct {
 	Text       string
 	Typed      []rune
 	Keystrokes int
+	Start      time.Time
+	Duration   time.Duration
 }
 
 func (tm TestModel) Init() tea.Cmd {
-	return nil
+	return tick()
 }
 
 func (tm TestModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	index := len(tm.Typed)
 	switch msg := msg.(type) {
+	case tickMsg:
+		if !tm.Start.IsZero() && time.Since(tm.Start) >= tm.Duration {
+			// TODO calculate the result
+			return tm, tea.Quit
+		}
+		return tm, tick()
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
@@ -38,6 +48,9 @@ func (tm TestModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			index--
 			tm.Typed = tm.Typed[:index]
 		default:
+			if tm.Start.IsZero() {
+				tm.Start = time.Now()
+			}
 			char := msg.String()[0]
 			index++
 			tm.Keystrokes++
@@ -55,6 +68,15 @@ func (tm TestModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (tm TestModel) View() string {
 	var out strings.Builder
 
+	left := tm.Duration
+	if !tm.Start.IsZero() {
+		left -= time.Since(tm.Start)
+	}
+	if left < 0 {
+		left = 0
+	}
+
+	out.WriteString(fmt.Sprintf("%d\n", int((left+time.Second-1)/time.Second)))
 	for i, r := range []rune(tm.Text) {
 		switch {
 		case i == len(tm.Typed):
@@ -69,4 +91,12 @@ func (tm TestModel) View() string {
 	}
 
 	return out.String()
+}
+
+type tickMsg time.Time
+
+func tick() tea.Cmd {
+	return tea.Tick(100*time.Millisecond, func(t time.Time) tea.Msg {
+		return tickMsg{}
+	})
 }
