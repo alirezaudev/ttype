ccd6cf3a064c224849162c1e5289b80e895539a9
Alireza Mousavi <mousaviali1381@gmail.com>
2026-09-07 20:18:44 +0400

    chore(contrib): add homebrew, aur and scoop packaging

---
+++ contrib/homebrew/ttype.rb
@@ -0,0 +1,41 @@
+# This file written by AI :)
+#
+# Homebrew formula for ttype. The version and sha256 values are rewritten by
+# the release workflow; do not edit them by hand.
+class Ttype < Formula
+  desc "Terminal-first typing practice"
+  homepage "https://github.com/alirezaudev/ttype"
+  version "0.0.0"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/alirezaudev/ttype/releases/download/v#{version}/ttype_#{version}_darwin_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/alirezaudev/ttype/releases/download/v#{version}/ttype_#{version}_darwin_amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/alirezaudev/ttype/releases/download/v#{version}/ttype_#{version}_linux_arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/alirezaudev/ttype/releases/download/v#{version}/ttype_#{version}_linux_amd64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "ttype"
+    man1.install "ttype.1" if File.exist?("ttype.1")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ttype --version")
+  end
+end
